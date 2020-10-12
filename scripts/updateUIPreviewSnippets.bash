d789e9b3da548602eccb6bc34fad6cf2da44822b
PutkovDimi <putkovdimi@gmail.com>
2020-10-12 22:27:22 +0300

    feature(1.0): update script for create templates of xcode previewable view files

--- scripts/updateUIPreviewSnippets.bash
+++ scripts/updateUIPreviewSnippets.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# create if needed and update code snippets for ui's preview
 
 mkdir -p $HOME/Library/Developer/Xcode/UserData/CodeSnippets
 cp -R ../PreviewCodeSnippets/. $HOME/Library/Developer/Xcode/UserData/CodeSnippets/
+
+# create if needed and update file's templates for ui's preview
+
+mkdir -p $HOME/Library/Developer/Xcode/Templates
+cp -R ../PreviewFileTemplates/UI\ Preview\ Supported\ Templates  $HOME/Library/Developer/Xcode/Templates/
